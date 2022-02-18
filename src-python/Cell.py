4919ee78c5aaa6e7372e1cc8bbe51bdcfb544cbf
Amirreza <ar.nayyeri99@gmail.com>
2022-02-18 23:03:01 +0330

    add boolean table and change explorer

--- src-python/Cell.py
+++ src-python/Cell.py
@@ -1,10 +1,12 @@
 class Cell:
-    def __init__(self, row: int, col: int, path: list, path_value: int, goal_value: int):
+    def __init__(self, row: int, col: int, table: list, path_value: int, goal_value: int, path: list):
         self.row = row
         self.col = col
         self.path_value = path_value
         self.path = path
         self.goal_value = goal_value
+        self.table = table
+        self.table[row][col] = True
 
     def __eq__(self, other):
         if type(other) != Cell:
@@ -13,11 +15,6 @@ class Cell:
             return True
         return False
 
-    def __get_sorted_path(self) -> list:
-        sorted_path = self.path.copy()
-        sorted_path.sort(key=lambda x: (x.row, x.col))
-        return sorted_path
-
     def __hash__(self):
-        return ' '.join([str(x) for x in self.__get_sorted_path()]) + '###' + str(self.path_value) + ' ' + str(
-            self.goal_value)
+        return ','.join(str(item) for innerlist in self.table for item in innerlist) + '@@@' + str(
+            self.row) + ' ' + str(self.col) + '###' + str(self.path_value) + ' ' + str(self.goal_value)
